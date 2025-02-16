01ef847f964f33fd94d1a1daffb70bccc9215081
MohamDahALU <m.dahab@alustudent.com>
2025-02-16 11:54:41 +0200

    commit for 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,21 +1,11 @@
 #!/usr/bin/python3
-"""
-This module contains a function to get the number
-of subscribers for a given subreddit.
-"""
+"""DOCUMENTATION"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Returns the number of subscribers for a given subreddit.
+    """DOCUMENTATION"""
 
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        int: The number of subscribers, or 0 if the subreddit is not found.
-    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
