5781eb54e45af27568f9bf3e709cc2e3a52287ef
MohamDahALU <m.dahab@alustudent.com>
2025-02-16 11:42:48 +0200

    commit for 1-top_ten.py

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+A module that prints the titles of the top
+10 hot posts from a subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    A function that fetches and prints the titles
+    of the top ten hot posts from a subreddit.
+    """
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json().get("data", {}).get("children", [])
+        for post in data:
+            print(post.get("data", {}).get("title"))
+    else:
+        print("None")
+
+
+print(top_ten("sudan"))
