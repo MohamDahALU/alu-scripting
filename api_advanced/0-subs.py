d6e13694fda92cef710d3710d27a6647a83650b2
MohamDahALU <m.dahab@alustudent.com>
2025-02-21 14:28:22 +0200

    commit for 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -11,4 +11,11 @@ def number_of_subscribers(subreddit):
     Takes a subreddit name, and returns the number of subscribers
     new line
     """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
 
+    return 0
