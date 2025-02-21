4abadfe8c2f9de0062ee7e52fd35d3aea9d7e206
MohamDahALU <m.dahab@alustudent.com>
2025-02-21 14:30:46 +0200

    commit for 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -12,7 +12,7 @@ def top_ten(subreddit):
     of the top ten hot posts from a subreddit.
     """
 
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
