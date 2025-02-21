c4a29f193d4f1459d96a1c44daf2d57b7b192717
MohamDahALU <m.dahab@alustudent.com>
2025-02-21 14:45:52 +0200

    commit for 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -13,7 +13,7 @@ def top_ten(subreddit):
     """
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    header = headers = {"User-Agent": "Mozilla/5.0"}
+    headers = headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
