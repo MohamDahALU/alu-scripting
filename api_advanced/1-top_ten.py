7848467d1eb8671864bc9d9f6b4e08313f99ea6d
MohamDahALU <m.dahab@alustudent.com>
2025-02-21 14:40:38 +0200

    commit for 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -15,16 +15,14 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     header = headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        json = response.json()
-        data = json["data"]["children"]
-
-        for post in data[:10]:
-            print(post.get("data", {}).get("title"))
-
-    else:
+    if response.status_code != 200:
         print(None)
+        return
+
+    json = response.json()
+    data = json["data"]["children"]
+    for post in data[:10]:
+        print(post.get("data", {}).get("title"))
 
 
-# top_ten("sudan")
+top_ten("sudan")
